0bb93ad7c771449bf0406525441150b1ad521fb8
kavod <brice.grichy@gmail.com>
2015-04-09 23:15:54 +0200

    Manage MultiParam

--- param.py
+++ param.py
@@ -5,9 +5,10 @@ import os,sys
 import json
 import re
 import getpass
+import copy
 	
 class PointCusto(object):
-	def __init__(self,id,type,multi=False,label=None,placeholder="",required=False,choices=[],condition=[],value=None,default=None):
+	def __init__(self,id,type,multi=False,label=None,placeholder="",required=False,choices=[],condition=[],default=None):
 		# ID
 		self.id = str(id)
 		
@@ -56,10 +57,7 @@ class PointCusto(object):
 			self.default = default
 		
 		# Value
-		if value is not None:
-			self.setValue(value)
-		else:
-			self.value = self.default
+		self.setValue(self.default)
 		
 	def __str__(self):
 		if self.type == 'password' and not self.multi:
@@ -74,8 +72,17 @@ class PointCusto(object):
 			value = str(value)
 		return '{0:25} {1}'.format(self.label+':',value)
 		
+	def __deepcopy__(self,memo):
+		newone = type(self)(id=self.id,type=self.type,multi=self.multi,label=self.label,placeholder=self.placeholder,required=self.required,default=self.default)
+		newone.choices = dict(self.choices)
+		newone.condition = list(self.condition)
+		newone.value = copy.deepcopy(self.value)
+		return newone
+		
 	def setValue(self,value):
-		if self.multi:
+		if value is None:
+			self.value = None
+		elif self.multi:
 			if not isinstance(value,list):
 				raise WrongValue('401',str(value) + ' not correct for ' + str(self.type) + ' (multi)')
 			for item in value:
@@ -93,17 +100,6 @@ class PointCusto(object):
 		
 	def toJSON(self):
 		return self.value
-		'''return {
-			"id":			self.id,
-			"type":			self.type,
-			"multi":		self.multi,
-			"label":		self.label,
-			"placeholder":	self.placeholder,
-			"required":		self.required,
-			"choices":		self.choices,
-			"condition":	self.condition,
-			"value":		self.value
-			}'''
 
 	def _validate(self,value,emptyAllowed=True):
 		if self.type == "text" or self.type == "password" or self.type == "file":
@@ -134,28 +130,19 @@ class PointCusto(object):
 			return int(value)
 			
 	def cliPrompt(self):
-		self.value = globalPrompt(self.placeholder,self.choices,self.multi,password=(self.type=='password'),mandatory=False,default=self.default)
-		'''if self.multi:
-			if self.choices is None: # Multichoice
-				self.value = promptList(self.placeholder,password=(self.type=='password'))
-			else: # Multientries
-				choicesList = [[key,val] for key,val in enumerate(self.choices)]
-				result = promptList(self.placeholder,choix=choicesList)
-				self.value = []
-				for item in result:
-					self.value.append(self.choices[item])
-		else:
-			if self.choices is None: # Single entry
-				self.value = promptSimple(self.placeholder,self.default,password=(self.type=='password'))
-			else: # Single choice
-				choicesList = [[key,val] for key,val in enumerate(self.choices)]
-				default = [i for i in enumerate(self.choices) if i[1] == self.default][0] if self.default is not None else 0
-				self.value = self.choices[promptChoice(self.placeholder,choix=choicesList,default=default)]'''
-			
+		warning = ''
+		while True:
+			reponse = globalPrompt(self.placeholder,self.choices,self.multi,password=(self.type=='password'),mandatory=False,default=self.default,warning=warning)	
+			if self._validate(reponse,not self.required):
+				self.value = reponse
+				break
+			warning='Incorrect answer'
+		
 			
 class Param(object):
 	def __init__(self,id,label="",items=[],filename=None):
 		self.id = str(id)
+		self.multi = False
 		self.label = str(label) if str(label) is not None else self.id
 		for it in items:
 			if not isinstance(it,PointCusto) and not isinstance(it,Param):
@@ -171,6 +158,11 @@ class Param(object):
 			result += str(item) + '\n'
 		return result
 		
+	def __deepcopy__(self,memo):
+		newone = type(self)(id=self.id,label=self.label,items=[],filename=self.filename)
+		newone.items = copy.deepcopy(self.items)
+		return newone
+		
 	def getType(self):
 		return "param"
 		
@@ -179,7 +171,7 @@ class Param(object):
 			raise IdAlreadyUsed('402',str(item.id) + ' already used as ID')
 		if not isinstance(item,PointCusto) and not isinstance(item,Param):
 			raise WrongValue('401',str(it) + ' not correct for Param or PointCusto')
-		self.items.append(item)
+		self.items.append(copy.deepcopy(item))
 		
 	def addItems(self,items):
 		if not isinstance(items,list):
@@ -191,7 +183,7 @@ class Param(object):
 		for key in json.keys():
 			for it in self.items:
 				if str(it.id) == str(key):
-					if it.getType() == 'param':
+					if isinstance(it,Param):
 						it.loadValuesFromJSON(json[key])
 					else:
 						it.setValue(json[key])
@@ -213,7 +205,79 @@ class Param(object):
 		for item in self.items:
 			result[item.id] = item.toJSON()
 		return result
+
+class ParamMulti(Param):
+	def __init__(self,id,label="",items=[],filename=None):
+		Param.__init__(self,id=id,label=label,items=[],filename=filename)
+		self.id = id
+		self.label = label
+		self.filename = filename
+		self.multi = True
+		self.prototype = []
+		for it in items:
+			if not isinstance(it,PointCusto) and not isinstance(it,Param):
+				raise WrongValue('401',str(it) + ' not correct for Param or PointCusto')
+		self.addItems(items)
 		
+	def __str__(self):
+		result = ''
+		for i,j in enumerate(self.items):
+			label = str(self.label) + ' ' + str(i+1)
+			result += label + '\n'
+			result += '-'*len(label) + '\n'
+			for item in j:
+				result += str(item) + '\n'
+		return result
+		
+	def __deepcopy__(self,memo):
+		newone = type(self)(id=self.id,label=self.label,items=[],filename=self.filename)
+		newone.prototype = copy.deepcopy(self.prototype)
+		return newone
+
+	def addItem(self,item):
+		if len([it for it in self.prototype if it.id == item.id])>0:
+			raise IdAlreadyUsed('402',str(item.id) + ' already used as ID')
+		if not isinstance(item,PointCusto) and not isinstance(item,Param):
+			raise WrongValue('401',str(it) + ' not correct for Param or PointCusto')
+		self.prototype.append(copy.deepcopy(item))
+		
+	def loadValuesFromJSON(self,json_list):
+		if not isinstance(json_list,list):
+			json_list = [json_list]
+		for json in json_list:
+			for key in json.keys():
+				for it in self.items:
+					if str(it.id) == str(key):
+						if isinstance(it,Param):
+							it.loadValuesFromJSON(json[key])
+
+						else:
+							it.setValue(json[key])
+						del json[key]
+						break
+			if len(json)>0:
+				raise WrongValue('403',str(json.keys()[0]) + ' not in ' + str(self.id))
+
+	def cliPrompt(self):
+		if len(self.prototype) < 1:
+			raise WrongValue('406','ParamMulti is empty')
+		while True:
+			newitem = copy.deepcopy(self.prototype)
+			self.items.append(newitem)
+			for item in newitem:
+				item.cliPrompt()
+			if not promptYN('Another {0}?'.format(self.label),default='n'):
+				break
+			
+	def toJSON(self):
+		result = []
+		for item in self.items:
+			val = {}
+			for value in item:
+				val[value.id] = value.toJSON()
+			result.append(val)
+		return result
+	
 def promptSimple(question,default = '',password=False):
 	"""
 		The ``promptSimple`` function
@@ -244,19 +308,21 @@ def promptText(question,default = None,selected=[],warning='',password=False,man
 			str_default = '[{0}]'.format(str(default))
 		print_question('{0} {1}'.format(str(question),str_default),warning,selected)
 		reponse = str(prompt(password))
+		if reponse == '' and default is not None:
+			return default
 		if reponse != '' or not mandatory:
 			return reponse
 		warning = "Mandatory answer"
 		
-def globalPrompt(question,choix=[],multi=False,password=False,mandatory=False,default=None):
+def globalPrompt(question,choix=[],multi=False,password=False,mandatory=False,default=None,warning=''):
 	if default is not None and multi:
 		raise WrongValue('405',str(id) + ': default value not compatible with multivalue.')
 	if multi:
-		return promptMulti(question,choix=choix,password=password,mandatory=mandatory)
+		return promptMulti(question,choix=choix,password=password,mandatory=mandatory,warning=str(warning))
 	else:
-		return promptSingle(question,choix=choix,password=password,mandatory=mandatory,default=default)
+		return promptSingle(question,choix=choix,password=password,mandatory=mandatory,default=default,warning=str(warning))
 		
-def promptSingle(question,choix=[],password=False,mandatory=False,default=None):
+def promptSingle(question,choix=[],password=False,mandatory=False,default=None,warning=''):
 	if len(choix)>0:
 		if isinstance(choix,list):
 			mydict = {}
@@ -264,11 +330,28 @@ def promptSingle(question,choix=[],password=False,mandatory=False,default=None):
 				mydict.update({i:j})
 		else:
 			mydict = choix
-		reponse = promptChoice(question,selected=[],choix=mydict,mandatory=mandatory,default=default)
+		
+		reponse = promptChoice(question,selected=[],warning=str(warning),choix=mydict,mandatory=mandatory,default=default)
 	else:
-		reponse = promptText(question,selected=[],password=password,mandatory=mandatory,default=default)
+		reponse = promptText(question,selected=[],warning=str(warning),password=password,mandatory=mandatory,default=default)
 	return reponse
-			
+
+def promptYN(question,default=None):
+	str_y = 'y'
+	str_n = 'n'
+	if default is None:
+		mandatory = True
+	else:
+		mandatory = False
+		if str(default).lower() not in ['y','n']:
+			default = 'n'
+		if str(default).lower() == 'y':
+			str_y = str_y.upper()
+		else:
+			str_n = str_n.upper()
+	reponse = promptSingle('{0} [{1}/{2}]'.format(str(question),str_y,str_n),choix=[],password=False,mandatory=mandatory,default=str(default))
+	return reponse.lower() == 'y'
+
 		
 def promptMulti(question,choix=[],password=False,mandatory=False):
 	reponse = None
@@ -375,7 +458,7 @@ def promptChoice(question,choix,warning='',selected=[],default = None,mandatory=
 				return None
 		elif not reponse.isdigit():
 			warning = "Incorrect answer"
-		elif int(reponse) < 1 or int(reponse) > len(choix)+1:
+		elif int(reponse) < 1 or int(reponse) > len(choix):
 			warning = "Incorrect answer"
 		else:
 			return choix[int(reponse)-1][0]
@@ -408,46 +491,21 @@ class IdAlreadyUsed(Exception):
 		return "Error "+str(self.value)+": "+str(self.error)
 			
 if __name__ == '__main__':
-	'''width = 100
-	print ('-'*9+'*')*5
-	p1 = PointCusto('name','text',label="Name",required=True,value="Niouf Niouf")
-	p2 = PointCusto('age','number',label="Age",required=True,default=24,placeholder="Please enter age of "+p1.value)
-	p2.cliPrompt()
-	print p2
-	print p2.getType()
-	param = Param('conf','Configuration',[p1,p2])
-	p3 = PointCusto('email','email',label="Email",required=True,value=u"sdfd@sg.fr")
-	p4 = PointCusto('password','password',label="Password",required=True,value="123456")
-	param.addItems([p3,p4])
-	p5 = PointCusto('fruits','text',multi=True,label="Favorite fruits",required=True,value=[u"banana","pineapple"])
-	p5.cliPrompt()
-	param.addItem(p5)
-	p6 = PointCusto('childAge','number',multi=True,label="Children ages",required=True,value=[2,6])
-	param.addItem(p6)
-	print param
-	param.loadValuesFromJSON({'name':'Niorf Niorf','email':'niorf@niorf.fr'})
-	print param
-	param.cliPrompt()
-	print param'''
+	p1 = PointCusto('id','text',multi=False,label='Torrents provider',placeholder="Indicate your torrents provider",required=True,choices={'t411':'T411','kickass':'KickAss'},condition=[],default='kickass')
+	p2 = PointCusto('user','text',multi=False,label='Torrents provider username',placeholder="Indicate your torrents provider user",required=False,choices=[],condition=[],default=None)
+	p3 = PointCusto('password','password',multi=False,label='Torrents provider password',placeholder="Indicate your torrents provider password",required=False,choices=[],condition=[],default=None)
+	param_tracker = ParamMulti(id='tracker',label='Torrent providers',items=[p1,p2,p3],filename=None)
 	
+	p4 = PointCusto('server','text',multi=False,label='Transmission server',placeholder="Indicate the transmission address",required=True,choices=[],condition=[],default=None)
+	p5 = PointCusto('port','number',multi=False,label='Transmission port',placeholder="Indicate the transmission port",required=True,choices=[],condition=[],default=51413)
+	p6 = PointCusto('user','text',multi=False,label='Transmission username',placeholder="Indicate the transmission username",required=False,choices=[],condition=[],default=None)
+	p7 = PointCusto('password','password',multi=False,label='Transmission password',placeholder="Indicate the transmission password",required=False,choices=[],condition=[],default=None)
+	p8 = PointCusto('slots','number',multi=False,label='Transmission maximum slots',placeholder="Indicate the maximum number of simultaneous slots",required=True,choices=[],condition=[],default=6)
+	p9 = PointCusto('transfer','text',multi=False,label='Local transfer directory',placeholder="Indicate the target local directory (keep blank for disable)",required=False,choices=[],condition=[],default=None)
+	param_transmission = Param(id='transmission',label='Transmission',items=[p4,p5,p6,p7,p8,p9],filename=None)
+
+	param = Param(id='conf',label="Configuration",items=[param_tracker,param_transmission],filename=None)
 	
-	p1 = PointCusto("id","text",multi=True,label="Torrent provider",placeholder="Indicate your torrent provider",required=False,choices={'t411':'T411','kickass':'Kickass'},condition=[],value=None,default=None)
-	p2 = PointCusto("username","text",multi=False,label="Torrent provider username",placeholder="Indicate the username for the tracker",required=False,choices=None,condition=[],value=None,default=None)
-	p3 = PointCusto("password","password",multi=False,label="Torrent provider password",placeholder="Indicate the password for the tracker",required=False,choices=None,condition=[],value=None,default=None)
-	param_tracker = Param('tracker','Tracker',[p1,p2,p3])
-	param = Param('conf','Configuration',[param_tracker])
 	param.cliPrompt()
-	print param
+	
 	print param.toJSON()
-	
-	'''print "Single, Not password, mandatory"
-	print promptSingle(question="Enter a name",default = "Niouf Niouf",password=False,mandatory=True)
-	print promptSingle(question="Enter a country",default = 0,password=False,mandatory=True,choix=['France','Other place'])
-	print promptSingle(question="Enter a password",password=True,mandatory=False)
-	print promptSingle(question="Your favorite football club",password=False,mandatory=False,choix=['PSG','OM'])
-	print promptMulti(question="Enter children names",password=False,mandatory=False)
-	print promptMulti(question="Enter at least one wish",password=False,mandatory=True)
-	#print promptChoice(question="What do you own?",choix={0:'Playstation',1:'Sega'},warning='',selected=[],default = None,mandatory=True)
-	print promptMulti(question="married?",password=False,mandatory=True,choix=['Yes','No', 'Yes but no longer'])
-	print promptMulti(question="Do you own?",password=False,mandatory=False,choix={'psx':'a Playstation','sega':'a Sega', 'nes':'a Nintendo'})'''
-	
